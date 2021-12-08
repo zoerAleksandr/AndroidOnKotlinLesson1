246441ee45bfda5d200b09b4db9d6058456454f6
zoer <zoer.aleksandr@gmail.com>
2021-12-08 22:24:11 +0300

    Сделал тестовое приложение для перевода на Kotlin

---
+++ app/src/main/java/com/example/androidonkotlinlesson1/BlankFragment.java
@@ -0,0 +1,27 @@
+package com.example.androidonkotlinlesson1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class BlankFragment extends Fragment {
+
+    public static BlankFragment newInstance() {
+        return new BlankFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
--- app/src/main/java/com/example/androidonkotlinlesson1/MainActivity.java
+++ app/src/main/java/com/example/androidonkotlinlesson1/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.androidonkotlinlesson1;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextInputEditText editText = findViewById(R.id.input_text);
+        Button btn = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.message);
+        Button btnFragment = findViewById(R.id.btn_fragment);
+
+        btn.setOnClickListener(v -> {
+            Editable message = editText.getText();
+
+            if (message == null) textView.setText("Введите текст");
+            else if (message.length() > 5) textView.setText(message);
+            else textView.setText("Введите более 5 символов");
+
+        });
+
+        btnFragment.setOnClickListener(v -> getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, BlankFragment.newInstance())
+                .addToBackStack("1")
+                .commit());
     }
 }
